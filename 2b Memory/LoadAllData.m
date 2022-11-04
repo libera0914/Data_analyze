f52b5fa30e6b47bf1a3642960ee16e1df8e8db16
libera0914 <920376891@qq.com>
2022-11-04 01:09:57 +0000

    delete

--- 2b Memory/LoadAllData.m
+++
@@ -1,9 +0,0 @@
-function [AllDataMem] = LoadAllData(FileName,Contents);
-for idx = 1: 8
-    AllData(idx,:,:,:) = ncread(FileName, Contents.Variables(idx).Name);
-    fprintf('Loading %s\n', Contents.Variables(idx).Name); % display loading information
-end
-
-AllDataMem = whos('AllData').bytes/1000000;
-fprintf('Memory used for all data: %.3f MB\n', AllDataMem)
-end
